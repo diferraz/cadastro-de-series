4d6b425929ce38293c31dd4084b099adc257d76b
diferraz <35599682+diferraz@users.noreply.github.com>
2022-01-29 19:36:25 -0300

    Enum

---
+++ Cenero.cs
@@ -0,0 +1,31 @@
+namespace cadastro_de_series
+{
+    public enum cenero
+    {
+        Acao = -1;
+
+        Aventura = -2;
+
+        Comedia = -3;
+
+        Documentario = -4;
+
+        Drama = -5;
+
+        Espionagem = -6;
+
+        Faroeste = -7;
+
+        Fantasia = -8;
+
+        Ficsao = -9;
+
+        Musical = -10;
+
+        Romance = -11;
+
+        Suspense = -12;
+
+        Terror = -13;
+    }
+}
